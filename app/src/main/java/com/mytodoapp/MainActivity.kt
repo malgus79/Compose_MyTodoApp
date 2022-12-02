70d977aa3e65ba618e95b4efe89284efdfde0b24
Gustavo Maldonado <gamcba7982@gmail.com>
2022-12-02 19:08:01 -0300

    Dagger Hilt added

--- app/src/main/java/com/mytodoapp/MainActivity.kt
+++ app/src/main/java/com/mytodoapp/MainActivity.kt
@@ -3,14 +3,21 @@ package com.mytodoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import com.mytodoapp.addtask.ui.TasksScreen
+import com.mytodoapp.addtask.ui.TasksViewModel
 import com.mytodoapp.ui.theme.MyTodoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val tasksViewModel: TasksViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    TasksScreen()
+                    TasksScreen(tasksViewModel)
                 }
             }
         }
--- app/src/main/java/com/mytodoapp/addtask/ui/TasksScreen.kt
+++ app/src/main/java/com/mytodoapp/addtask/ui/TasksScreen.kt
@@ -3,6 +3,6 @@ package com.mytodoapp.addtask.ui
 import androidx.compose.runtime.Composable
 
 @Composable
-fun TasksScreen() {
+fun TasksScreen(tasksViewModel: TasksViewModel) {
 
 }
